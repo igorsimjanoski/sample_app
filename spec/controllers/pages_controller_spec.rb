0f1bb4a1f13fdc91d3a7b5ea1b82c87902eb39e2
igorsimjanoski <ige.simjanoski@gmail.com>
2012-01-15 14:31:46 +0100

    finished layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+	
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -10,7 +11,7 @@ describe PagesController do
 
     it "should have right title" do
 	get 'home'
-	response.should have_selector("title",
+	response.should have_selector('title',
 				:content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
@@ -23,7 +24,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
+      response.should have_selector('title',
 		:content =>"Ruby on Rails Tutorial Sample App | Contact")
     end
 
@@ -37,7 +38,7 @@ describe PagesController do
     
     it "should have the right title" do
         get 'about'
-	response.should have_selector("title",
+	response.should have_selector('title',
 	:content => "Ruby on Rails Tutorial Sample App | About")
      end
 
